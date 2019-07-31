7f673eaab6ca61b135bad731e63a31d89ee42b4e
shereen <shereenamer20@icloud.com>
2019-07-31 07:57:23 +0200

    Initial Commit

---
+++ dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dicee
+//
+//  Created by shereen on 7/31/19.
+//  Copyright © 2019 shereen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
